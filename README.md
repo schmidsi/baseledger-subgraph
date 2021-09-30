bfbf78cd3df9db48cfbb4adb5d212306abac9a0f
Simon Emanuel Schmid <simon@edgeandnode.com>
2021-09-30 19:42:17 -0400

    Add Account entity

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 Contract to index: https://ropsten.etherscan.io/address/0x0B5FC75192F8EE3B4795AB44b3B455aB3d97A6dF#code
 
 ## Step by step
@@ -54,12 +53,28 @@ To have a first quickwin, we will deploy the boilerplate.
 graph auth --product hosted-service <ACCESS_TOKEN>
 ```
 
+### Play around
+
+https://graphiql-online.com/graphiql
+
 ### Add Solidty file to project
 
 This is not necessary, but it is a good practice to have the solidity file in your project.
 
-## Further reading
+### Enhance the schema
 
+By inspecting the data and code it makes sense to create an account entity to aggregate the stake and more.
+
+```graphql
+type Account @entity {
+  id: ID!
+  staked: BigInt! # uint256
+}
+```
+
+## Other resources
+
+https://graphiql-online.com/graphiql
 https://github.com/dabit3/building-a-subgraph-workshop
 https://thegraph.com/docs/developer/quick-start
 https://thegraph.com/discord
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,38 +1,48 @@
 import {
   Deposit as DepositEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
-  Withdraw as WithdrawEvent
-} from "../generated/Staking/Staking"
-import { Deposit, OwnershipTransferred, Withdraw } from "../generated/schema"
+  Withdraw as WithdrawEvent,
+} from "../generated/Staking/Staking";
+import { Account, Deposit, Withdraw } from "../generated/schema";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+
+function getOrCreateAccount(address: Address): Account {
+  let account = Account.load(address.toHexString());
+  if (account == null) {
+    account = new Account(address.toHexString());
+    account.staked = BigInt.fromI32(0);
+    account.save();
+  }
+  return account as Account;
+}
 
 export function handleDeposit(event: DepositEvent): void {
   let entity = new Deposit(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.addr = event.params.addr
-  entity.beneficiary = event.params.beneficiary
-  entity.validator = event.params.validator
-  entity.amount = event.params.amount
-  entity.save()
-}
+  );
+  let beneficiary = getOrCreateAccount(event.params.beneficiary);
+  beneficiary.staked = beneficiary.staked.plus(event.params.amount);
+  beneficiary.save();
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
+  entity.sender = getOrCreateAccount(event.params.addr).id;
+  entity.beneficiary = beneficiary.id;
+  entity.validator = event.params.validator;
+  entity.amount = event.params.amount;
+  entity.save();
 }
 
 export function handleWithdraw(event: WithdrawEvent): void {
   let entity = new Withdraw(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.addr = event.params.addr
-  entity.validator = event.params.validator
-  entity.amount = event.params.amount
-  entity.save()
+  );
+
+  let beneficiary = getOrCreateAccount(event.params.addr);
+  beneficiary.staked = beneficiary.staked.minus(event.params.amount);
+  beneficiary.save();
+
+  entity.beneficiary = beneficiary.id;
+  entity.sender = beneficiary.id; // For the sake of similarity
+  entity.validator = event.params.validator;
+  entity.amount = event.params.amount;
+  entity.save();
 }
